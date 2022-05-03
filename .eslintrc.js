a7b576c5754e900a0fc7853688b9a5e6ccb35209
lambda-chat <chat.algebrique@gmail.com>
2022-05-03 22:54:32 +0900

    disable react/require-default-props rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -103,6 +103,7 @@ module.exports = {
     ],
     'react/react-in-jsx-scope': 'off',
     'react/function-component-definition': 'off',
+    'react/require-default-props': 'off',
   },
   overrides: [
     {
